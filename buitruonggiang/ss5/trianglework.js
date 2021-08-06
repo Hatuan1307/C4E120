c2cfde050c4f49253c9cc030d9d672c832f92d8c
unknown <truonggiang66866@gmail.com>
2021-08-06 02:57:48 -0600

    Bai tap ve nha

---
+++ buitruonggiang/ss5/trianglework.js
@@ -0,0 +1,17 @@
+
+
+
+for(let i = 0; i <= 6; i++){
+    document.write("</br>");
+    for(let j= 0; j <i; j++){
+        document.write("&nbsp&nbsp");
+    }
+    for(let j = 0; j< 6-i; j++){
+        document.write("*");
+    }
+}
+
+
+
+
+
